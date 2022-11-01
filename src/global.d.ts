9ba1b8eac4d56dc9ca3297a65725a047c1762e50
Nikolai Fomin <kk309@mail.ru>
2022-11-01 23:19:20 +0300

    correct d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -6,3 +6,25 @@ declare type VoidFunc<T> = (...arg: T[]) => void;
 declare type EventFunc<T> = (event: T) => void;
 
 declare module "react-drag-drop-container";
+
+declare interface IGameTheme extends DefaultTheme {
+  panel: {
+    slot: string;
+    bg: string;
+  };
+  body: {
+    bgColor: string;
+    decor: {
+      left: string;
+      right: string;
+    };
+    items: string[];
+  };
+  itemsDecor?: IThemeBodyDecorItem[];
+}
+
+declare interface IThemeBodyDecorItem {
+  src: string;
+  top: string;
+  left: string;
+}
